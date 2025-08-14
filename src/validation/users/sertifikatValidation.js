96f58de00a8739c058abf5083ad0efdac7023b24
Jofanza Denis <aldidajofanzadenis@gmail.com>
2025-08-14 14:06:26 +0700

    Delete src directory

--- src/validation/users/sertifikatValidation.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-export const sertifikatSchema = Joi.object({
-  certificate_number: Joi.string().min(5).max(100).required(),
-  description: Joi.string().optional(),
-  issued_date: Joi.date().iso().required()
-});
